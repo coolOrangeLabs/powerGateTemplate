f8ec888fc2731e31ad6f8cf6a943918066d03743
Christian Gessner <christian.gessner@coolorange.com>
2020-03-13 21:51:30 +0100

    Batch file to distribute files from VS to final destination

---
+++ Files/make.cmd
@@ -0,0 +1,9 @@
+xcopy /Y /E /I "DataStandard\CAD.Custom" "C:\ProgramData\Autodesk\Vault 2020\Extensions\DataStandard\CAD.Custom"
+xcopy /Y /E /I "DataStandard\Vault.Custom" "C:\ProgramData\Autodesk\Vault 2020\Extensions\DataStandard\Vault.Custom"
+
+xcopy /Y /E /I "powerGate" "C:\ProgramData\coolOrange\powerGate"
+
+xcopy /Y /E /I "powerEvents" "C:\ProgramData\coolOrange\powerEvents"
+xcopy /Y /E /I "powerJobs" "C:\ProgramData\coolOrange\powerJobs"
+
+pause
