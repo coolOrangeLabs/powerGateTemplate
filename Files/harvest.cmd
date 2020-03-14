a1eb8ebd7cb98613aa594ccf27d63c87b737a319
Christian Gessner <christian.gessner@coolorange.com>
2020-03-14 13:21:45 +0100

    Batch file to harvest files back to VS

---
+++ Files/harvest.cmd
@@ -0,0 +1,9 @@
+xcopy /Y /E /I "C:\ProgramData\Autodesk\Vault 2020\Extensions\DataStandard\CAD.Custom" "DataStandard\CAD.Custom" /EXCLUDE:harvest.exclude
+xcopy /Y /E /I "C:\ProgramData\Autodesk\Vault 2020\Extensions\DataStandard\Vault.Custom" "DataStandard\Vault.Custom" 
+
+xcopy /Y /E /I "C:\ProgramData\coolOrange\powerGate" "powerGate"
+
+rem xcopy /Y /E /I "C:\ProgramData\coolOrange\powerEvents" "powerEvents"
+rem xcopy /Y /E /I "C:\ProgramData\coolOrange\powerJobs" "powerJobs"
+
+pause
