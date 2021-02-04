62a15117cf0ab116c727b23d0a6f38418e1291c4
Patrick Gruber <patrick.gruber@coolorange.com>
2021-02-04 14:00:11 +0100

    Added Validation module for ProAlpha

---
+++ Files/powerEvents/Modules/SAP.Checks.psm1
@@ -0,0 +1,4 @@
+function Verify-VaultRestrictionWhenSapChangeNumberIsNotOk {
+ # 
+https://github.com/coolOrangeProjects/Metso-Minerals/blob/d29732ab28b1f359ae0e36dc1a4838da8dc26a91/Files/coolOrange/powerEvents/Events/stateChangeRestriction.ps1
+} 
