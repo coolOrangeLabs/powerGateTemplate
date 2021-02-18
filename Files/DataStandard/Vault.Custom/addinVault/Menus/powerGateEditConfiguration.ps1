ec73f33a058a97606433ca577aa66871c438763e
Patrick Gruber <patrick.gruber@coolorange.com>
2021-02-18 21:19:57 +0000

    Fixed edit+save config

--- Files/DataStandard/Vault.Custom/addinVault/Menus/powerGateEditConfiguration.ps1
+++ Files/DataStandard/Vault.Custom/addinVault/Menus/powerGateEditConfiguration.ps1
@@ -1,11 +1,16 @@
 ﻿$global:ErrorActionPreference = "Stop"
-$commonModulePath = "C:\ProgramData\coolOrange\powerGate\Modules"
-$modules = Get-ChildItem -path $commonModulePath -Recurse -Filter *.ps*
-$modules | ForEach-Object { Import-Module -Name $_.FullName -Global }
+try {    
+    $commonModulePath = "C:\ProgramData\coolOrange\powerGate\Modules"
+    $modules = Get-ChildItem -path $commonModulePath -Recurse -Filter *.ps*
+    $modules | ForEach-Object { Import-Module -Name $_.FullName -Global }
 
-$cfgPath = "c:\temp\powerGateCfg\powerGateConfiguration.xml"
-[xml]$cfg = Get-PowerGateConfigFromVault
-New-Item -Path "c:\temp\powerGateCfg" -ItemType Directory -Force
-$cfg.Save($cfgPath)
+    $cfgPath = "c:\temp\powerGateCfg\powerGateConfiguration.xml"
+    [xml]$cfg = Get-PowerGateConfigFromVault
+    New-Item -Path "c:\temp\powerGateCfg" -ItemType Directory -Force
+    $cfg.Save($cfgPath)
 
-Start-Process -FilePath C:\Windows\explorer.exe -ArgumentList "/select, ""$cfgPath"""
+    Start-Process -FilePath C:\Windows\explorer.exe -ArgumentList "/select, ""$cfgPath"""
+}
+catch {
+    (ShowMessageBox -Message $_.Exception.Message -Button  "OK" -Icon "Error") | Out-Null
+}
--- Files/DataStandard/Vault.Custom/addinVault/Menus/powerGateSaveConfiguration.ps1
+++ Files/DataStandard/Vault.Custom/addinVault/Menus/powerGateSaveConfiguration.ps1
@@ -1,17 +1,22 @@
 ﻿$global:ErrorActionPreference = "Stop"
-$commonModulePath = "C:\ProgramData\coolOrange\powerGate\Modules"
-$modules = Get-ChildItem -path $commonModulePath -Recurse -Filter *.ps*
-$modules | ForEach-Object { Import-Module -Name $_.FullName -Global }
+try {  
+    $commonModulePath = "C:\ProgramData\coolOrange\powerGate\Modules"
+    $modules = Get-ChildItem -path $commonModulePath -Recurse -Filter *.ps*
+    $modules | ForEach-Object { Import-Module -Name $_.FullName -Global }
 
-$cfgPath = "c:\temp\powerGateCfg\powerGateConfiguration.xml"
-$testPath = Test-Path $cfgPath
-if ($testPath -eq $false) {
-    ShowMessageBox -Message "No config file found in '$cfgPath' `n Please download/edit the config file first and then save it back to the Vault server!" -Button  "OK" -Icon "Warning" | Out-Null
-    return
+    $cfgPath = "c:\temp\powerGateCfg\powerGateConfiguration.xml"
+    $testPath = Test-Path $cfgPath
+    if ($testPath -eq $false) {
+        ShowMessageBox -Message "No config file found in '$cfgPath' `n Please download/edit the config file first and then save it back to the Vault server!" -Button  "OK" -Icon "Warning" | Out-Null
+        return
+    }
+
+    [byte[]]$cfg = [System.IO.File]::ReadAllBytes($cfgPath)
+    Set-PowerGateConfigFromVault -Content $cfg
+    ShowMessageBox -Message "Config file saved to Vault server" -Button "OK" -Icon "Information" | Out-Null
+
+    Remove-Item $cfgPath -Force
 }
-
-[byte[]]$cfg = [System.IO.File]::ReadAllBytes($cfgPath)
-Set-PowerGateConfigFromVault -Content $cfg
-ShowMessageBox -Message "Config file saved to Vault server" -Button "OK" -Icon "Information" | Out-Null
-
-Remove-Item $cfgPath -Force
+catch {
+    (ShowMessageBox -Message $_.Exception.Message -Button  "OK" -Icon "Error") | Out-Null
+}
