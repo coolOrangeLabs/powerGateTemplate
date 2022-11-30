674f655462814eb4912bc0bff700d4225dfdddd6
martinweiss92 <git@coolorange.com>
2022-11-30 12:10:24 +0100

    PG-1260   + adapted TransferERPItemViaItemTab script with duplicated logic from similar File Tab script

--- Files/powerEvents/Events/TransferERPItemViaItemTab.ps1
+++ Files/powerEvents/Events/TransferERPItemViaItemTab.ps1
@@ -1,39 +1,34 @@
-#region TO BE REMOVED
-if((Get-Process -Id $PID).ProcessName -eq 'powershell' -and $host.Name.StartsWith('powerEvents')){
-	return
-}
-if([System.Threading.Thread]::CurrentThread.GetApartmentState() -eq 'MTA' -and (Get-Process -Id $PID).ProcessName -ne 'powershell'){
-	Start-Process powershell.exe -ArgumentList "-STA",$MyInvocation.InvocationName -WindowStyle hidden
-	return
-}
+#region Debugging
+if((Get-Process -Id $PID).ProcessName -in @('powershell','powershell_ise')){
+	Import-Module powerEvents
+	
+	Open-VaultConnection -Server $env:Computername -Vault Vault -User Administrator -Password ""
+	$selectedItem = Get-VaultItem -Number 'co-00000'
+	
+	function Add-VaultTab($name, $EntityType, $Action){
+		Add-Type -AssemblyName PresentationFramework
 
-Import-Module powerEvents
-Open-VaultConnection -Server $env:Computername -Vault Vault -User Administrator -Password ""
-$selectedItem = Get-VaultItem -Number 'co-00000'
-
-function Add-VaultTab($name, $EntityType, $Action){
-	Add-Type -AssemblyName PresentationFramework
-
-	$xamlReader = New-Object System.Xml.XmlNodeReader ([xml]@'
-	<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-		xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
-		<Window.Resources>
-			<Style TargetType="{x:Type Window}">
-				<Setter Property="FontFamily" Value="Segoe UI" />
-				<Setter Property="Background" Value="#FFFDFDFD" />
-			</Style>
-		</Window.Resources>
-	</Window>
+		$xamlReader = New-Object System.Xml.XmlNodeReader ([xml]@'
+		<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+			xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
+			<Window.Resources>
+				<Style TargetType="{x:Type Window}">
+					<Setter Property="FontFamily" Value="Segoe UI" />
+					<Setter Property="Background" Value="#FFFDFDFD" />
+				</Style>
+			</Window.Resources>
+		</Window>
 '@)
-	$debugERPTab_window = [Windows.Markup.XamlReader]::Load($xamlReader)
-	$debugERPTab_window.Title = "powerGate Debug Window for Tab: $name"
-	$debugERPTab_window.AddChild($action.InvokeReturnAsIs($selectedItem))
-	$debugERPTab_window.ShowDialog()
+		$debugERPTab_window = [Windows.Markup.XamlReader]::Load($xamlReader)
+		$debugERPTab_window.Title = "powerGate Debug Window for Tab: $name"
+		$debugERPTab_window.AddChild($action.InvokeReturnAsIs($selectedItem))
+		$debugERPTab_window.ShowDialog()
+	}
+
+
+	Import-Module powergate
+	Connect-ERP -Service 'http://localhost:8080/PGS/ErpServices'
 }
-
-
-Import-Module powergate
-Connect-ERP -Service 'http://thomas-rossi:8080/PGS/ErpServices'
 #endregion
 
 $global:addinPath = $PSScriptRoot
@@ -45,100 +40,96 @@ $logPath = Join-Path $env:LOCALAPPDATA "coolOrange\Projects\VDS_Vault-powerGate.
 Set-LogFilePath -Path $logPath
 
 
-Add-VaultTab -Name 'ERP Item' -EntityType 'File' -Action {
+Add-VaultTab -Name 'ERP Item' -EntityType 'Item' -Action {
 	param($selectedItem)
-
-	$Script:erpItemTab_control = [Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::new([xml](Get-Content "$PSScriptRoot\ERPItem_Tab.xaml")))
-
-	#region Initialize viewmodel
-	$materialTabContext = New-Object -Type PsObject -Property @{
-		ErpEntity = $null
-		VaultEntity = $selectedItem
-		IsCreate = $false
-
-		Lists = @{
-			UomList = @(GetUnitOfMeasuresList)
-			MaterialTypeList = @(GetMaterialTypeList)
-			CategoryList = @(GetCategoryList)
-		}
-	}
-	#endregion Initialize viewmodel
-
-
-	#region Initialize UI Components
-	#region Register Validate tab events
-	$erpItemTab_Description = $erpItemTab_control.FindName('Description')
-	$erpItemTab_Description.Add_TextChanged({
-		param($Sender)
-
-		ValidateErpMaterialTab -ErpItemTab $Script:erpItemTab_control
-	})
-
-	$erpItemTab_MaterialTypeList = $Script:erpItemTab_control.FindName('MaterialTypeList')
-	$erpItemTab_MaterialTypeList.Add_SelectionChanged({
-		param($Sender)
-
-		ValidateErpMaterialTab -ErpItemTab $Script:erpItemTab_control
-	})
-	#endregion Register Validate tab events
-
-	$erpItemTab_LinkMaterialButton = $Script:erpItemTab_control.FindName("LinkMaterialButton")
-	$erpItemTab_LinkMaterialButton.IsEnabled = (IsEntityUnlocked -Entity $selectedItem)
-	$erpItemTab_LinkMaterialButton.Add_Click({
-		param($Sender, $EventArgs)
-
-		LinkErpMaterial -ErpItemTab $Script:erpItemTab_control
-	})
-
-	$erpItemTab_CreateOrUpdateMaterialButton = $Script:erpItemTab_control.FindName("CreateOrUpdateMaterialButton")
-	$erpItemTab_CreateOrUpdateMaterialButton.Add_Click({
-		param($Sender)
-
-		CreateOrUpdateErpMaterial -MaterialTabContext $Sender.DataContext
-	})
-
-	$erpItemTab_GoToMaterialButton = $Script:erpItemTab_control.FindName("GoToMaterialButton")
-	$erpItemTab_GoToMaterialButton.IsEnabled = $true
-	$erpItemTab_GoToMaterialButton.Add_Click({
-		param($Sender)
-
-		GoToErpMaterial -MaterialTabContext $Sender.DataContext
-	})
-	#endregion Initialize UI Components
-
-
+	$erpItemTab_control = [Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::new([xml](Get-Content "$PSScriptRoot\ERPItem_Tab.xaml")))
+	
+	$statusMessage_label = $erpItemTab_control.FindName("lblStatusMessage")
 	$erpServices = Get-ERPServices -Available
 	if (-not $erpServices) {
-		$Script:erpItemTab_control.FindName("lblStatusMessage").Content = "One or more services are not available!"
-		$Script:erpItemTab_control.FindName("lblStatusMessage").Foreground = "Red"
-		$Script:erpItemTab_control.IsEnabled = $false
-		return $Script:erpItemTab_control
+		$statusMessage_label.Content = "One or more services are not available!"
+		$statusMessage_label.Foreground = "Red"
+		$erpItemTab_control.IsEnabled = $false
+		return $erpItemTab_control
 	}
+
+	$unitOfMeasure_combobox = $erpItemTab_control.FindName('UomList')
+	$unitOfMeasure_combobox.ItemsSource = @(GetUnitOfMeasuresList)
+
+	$materialTypes_combobox = $erpItemTab_control.FindName('MaterialTypeList')
+	$materialTypes_combobox.ItemsSource = @(GetMaterialTypeList)
+
+	$categories_combobox = $erpItemTab_control.FindName('CategoryList')
+	$categories_combobox.ItemsSource = @(GetCategoryList)
+
+	$erpItemTab_CreateOrUpdateMaterialButton = $erpItemTab_control.FindName('CreateOrUpdateMaterialButton')
+	$erpItemTab_GoToMaterialButton = $erpItemTab_control.FindName('GoToMaterialButton')
 
 	$number = GetEntityNumber -entity $selectedItem
 	$erpMaterial = Get-ERPObject -EntitySet "Materials" -Keys @{ Number = $number }
 	if(-not $?) {
-		$Script:erpItemTab_control.FindName("lblStatusMessage").Content = $Error[0]
-		$Script:erpItemTab_control.FindName("lblStatusMessage").Foreground = "Red"
-		$Script:erpItemTab_control.IsEnabled = $false
-		return $Script:erpItemTab_control
+		$statusMessage_label.Content = $Error[0]
+		$statusMessage_label.Foreground = "Red"
+		$erpItemTab_control.IsEnabled = $false
+		return $erpItemTab_control
 	}
-
+	
 	if (-not $erpMaterial) {
+		$statusMessage_label.Content = 'ERP: Create Material'
+
+		$erpItemTab_CreateOrUpdateMaterialButton.Content = 'Create ERP Item'
+		
+		$erpItemTab_GoToMaterialButton.IsEnabled = $false
+
 		$erpMaterial = NewErpMaterial
 		$erpMaterial = PrepareErpMaterialForCreate -erpMaterial $erpMaterial -vaultEntity $selectedItem
-		$materialTabContext.IsCreate = $true
-		$materialTabContext.ErpEntity = $erpMaterial
-		$Script:erpItemTab_control.FindName("GoToMaterialButton").IsEnabled = $false
 	}
 	else {
-		$materialTabContext.ErpEntity = $erpMaterial
-		$Script:erpItemTab_control.FindName("GoToMaterialButton").IsEnabled = $true
+		$statusMessage_label.Content = 'ERP: View/Update Material'
+
+		$erpItemTab_CreateOrUpdateMaterialButton.Content = 'Update ERP Item'
+
+		$erpItemTab_control.FindName('ModifiedDateLabel').Visibility = 'Visible'
+		$erpItemTab_control.FindName('ModifiedDateTextBox').Visibility = 'Visible'
+
+		$erpItemTab_GoToMaterialButton.Add_Click({
+			param($Sender)
+	
+			GoToErpMaterial -MaterialTabContext $erpMaterial
+		})
+
+		$unitOfMeasure_combobox.IsEnabled = $false
+		$materialTypes_combobox.IsEnabled = $false
 	}
+	$erpItemTab_control.DataContext = $erpMaterial
 
-	$Script:erpItemTab_control.DataContext = $materialTabContext
-	ValidateErpMaterialTab -ErpItemTab $Script:erpItemTab_control
-	#$materialTabContext.IsCreate = $true
-	return $Script:erpItemTab_control
+	$erpItemTab_CreateOrUpdateMaterialButton.Add_Click({
+		param($Sender)
 
+		CreateOrUpdateErpMaterial -MaterialTabContext $erpMaterial
+	})
+
+	$erpItemTab_LinkMaterialButton = $erpItemTab_control.FindName('LinkMaterialButton')
+	$erpItemTab_LinkMaterialButton.IsEnabled = (IsEntityUnlocked -Entity $selectedItem)
+	$erpItemTab_LinkMaterialButton.Add_Click({
+		param($Sender, $EventArgs)
+
+		LinkErpMaterial -ErpItemTab $erpItemTab_control
+	})
+
+	$materialTypes_combobox.Add_SelectionChanged({
+		param($Sender)
+
+		ValidateErpMaterialTab -ErpItemTab $erpItemTab_control
+	})
+
+	$erpItemTab_Description = $erpItemTab_control.FindName('Description')
+	$erpItemTab_Description.Add_TextChanged({
+		param($Sender)
+
+		ValidateErpMaterialTab -ErpItemTab $erpItemTab_control
+	})
+
+	ValidateErpMaterialTab -ErpItemTab $erpItemTab_control
+	return $erpItemTab_control
 }
